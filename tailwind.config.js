0c61e156daef1d65264d5977a80fb30096bd1b68
ashish8449 <ashishgururani8449@gmail.com>
2022-10-16 22:56:07 +0530

    Created folder Structure for frontEnd

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      indigo: {
+        light: '#b3bcf5',
+        DEFAULT: '#5c6ac4',
+        dark: '#202e78',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
